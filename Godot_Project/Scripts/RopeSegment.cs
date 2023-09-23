814efb3db313d31bdbd131893bbf880cf9c7c292
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-09-23 22:37:21 +0200

    Rope physics added

---
+++ Godot_Project/Scripts/RopeSegment.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+
+public partial class RopeSegment : Node2D
+{
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        
+    }
+
+}
